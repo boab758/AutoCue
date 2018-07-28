e6d663b4c43ea9acca8b4d910ede9a636d79ff3f
Samuel <samuel@Samuels-iMac.local>
2018-07-28 23:14:32 +0800

    Empty text field no longer crashes

--- Speech/CopyPasteViewController.swift
+++ Speech/CopyPasteViewController.swift
@@ -7,27 +7,48 @@
 //
 
 import UIKit
+import Foundation
 
 class CopyPasteViewController: UIViewController {
 
     var modelController = ModelController()
+    var errorCard = CardView()
+    var errorOcc = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    @IBOutlet weak var copyPaste: UITextView! 
     
+    override func shouldPerformSegue(withIdentifier identifier: String?, sender: Any?) -> Bool {
+        return false
+    }
+
+    @IBOutlet weak var copyPaste: UITextView!
     
     
     @IBAction func saveSpeech(_ sender: UIButton) {
         var matchVC = self.modelController.match
-        matchVC.fakeInit(document: copyPaste.text)
-        modelController.color = "orange"
+        if errorOcc {
+            errorCard.removeFromSuperview()
+            errorOcc = false
+        }
+        if copyPaste.text == nil || copyPaste.text == "" {
+            errorCard = CardView(frame: CGRect(x:45, y:650, width: 270, height: 70)) //x:400/70y:200/340
+            errorCard.backgroundColor = UIColor.red
+            errorCard.setString(str: "Where is your speech?")
+            self.view.addSubview(errorCard)
+            errorOcc = true
+        } else {
+            matchVC.fakeInit(document: copyPaste.text)
+            modelController.color = "orange"
+            self.performSegue(withIdentifier: "showCueCard", sender: Any?)
+        }
         
     }
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
